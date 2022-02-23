8365167fbff0d509c820cc10afb635a7b2aa1da9
yongjun <ansdydrn8985@gmail.com>
2022-02-23 18:13:19 +0900

    Secondary commit

---
+++ src/main/java/com/maroonest/blog/hello/hello.java
@@ -0,0 +1,19 @@
+package com.maroonest.blog.hello;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello my app";
+    }
+
+
+}
